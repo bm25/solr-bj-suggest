501ab8421b5f2cc6d1b4368a964a251df88aa83c
amoiseenko <amoiseenko@griddynamics.com>
2019-10-23 19:15:08 +0300

    TokenStreams was replaced with StandardTokenizer

--- src/main/java/com/gd/bjq/suggest/search/ConceptQParser.java
+++ src/main/java/com/gd/bjq/suggest/search/ConceptQParser.java
@@ -2,7 +2,10 @@ package com.gd.bjq.suggest.search;
 
 import com.gd.bjq.suggest.search.query.MaxSubConceptMatchQueryBuilder;
 import com.gd.bjq.suggest.search.query.util.SequentialWordCombinationsGenerator;
-import com.google.common.collect.Lists;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.search.*;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.core.SolrCore;
@@ -10,11 +13,14 @@ import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.search.QParser;
 import org.apache.solr.search.SyntaxError;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ConceptQParser extends QParser {
-    protected List<String> tokenizedQuery;
+    private static final String TOKEN_STREAM_DEFAULT_FIELD_NAME = "BRAND";
 
     public ConceptQParser(String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req) {
         super(qstr, localParams, params, req);
@@ -22,7 +28,17 @@ public class ConceptQParser extends QParser {
 
     @Override
     public Query parse() throws SyntaxError {
-        tokenizedQuery = tokenizeSearchQuery(qstr);
+        List<String> tokenizedQuery = new ArrayList<>();
+        Analyzer analyzer = req.getSchema().getQueryAnalyzer();
+        try (TokenStream stream = analyzer.tokenStream(TOKEN_STREAM_DEFAULT_FIELD_NAME,new StringReader(qstr))) {
+            stream.reset();
+            while (stream.incrementToken()) {
+                tokenizedQuery.add(stream.getAttribute(CharTermAttribute.class).toString());
+            }
+        } catch (IOException e) {
+            throw new SyntaxError(e);
+        }
+
         return buildQuery(tokenizedQuery);
     }
 
@@ -40,9 +56,5 @@ public class ConceptQParser extends QParser {
 
     private boolean isServiceField(String fieldName){
         return "id".equals(fieldName) || fieldName.charAt(0) == '_';
-    }
-
-    public static List<String> tokenizeSearchQuery(final String qString) {
-        return Lists.newArrayList(TokenStreams.tokenizeWhitespace(qString));
     }
 }
